00caff8474d1f65e1672997af8b6f3c076c819df
drop out <tnmitech@gmail.com>
2025-03-20 14:53:14 +0800

    Add AuthenticationSystem/DTOs/AuthResponse.cs 1742453593194

---
+++ AuthenticationSystem/DTOs/AuthResponse.cs
@@ -0,0 +1,9 @@
+namespace AuthenticationSystem.DTOs;
+
+public class AuthResponse
+{
+    public bool Success { get; set; }
+    public string? Token { get; set; }
+    public string? Message { get; set; }
+    public List<string>? Roles { get; set; }
+}
